5e751c41a9fa5ba3d63330a623fc036db122d8d8
awrelyah <aureliamihkk@gmail.com>
2022-10-12 18:55:36 +0300

    style changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,13 @@ import React from 'react'
 function Header(props) {
   return (
     <div className='header'>
-        <div className='title'>MEMORY GAME</div>
+      <div>
+      <div className='title'>MEMORY GAME</div>
         <div>Click on different characters and test your memory!</div>
+      </div>
         <div className='scores'>
-            <div>current score - {props.score.current}</div>
-            <div>max score - {props.score.max}</div>
+            <div className='score'>current score - {props.score.current}</div>
+            <div className='score'>max score - {props.score.max}</div>
         </div>
     </div>
   )
